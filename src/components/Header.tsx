b149639d7269f1fd42a669ac8da5902d8ce087c1
Williams Hernández <hernandw@gmail.com>
2024-08-20 18:31:19 -0400

    :tada: inicio del proyecto FoodApp

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <header className="bg-green-800 py-5">
+<h1 className="text-4xl font-black text-white text-center">Calculadora de Propinas y Consumos</h1>
+    </header>
+  )
+}
+
+export default Header
